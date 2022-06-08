24d4f96640dbaff3e1c4f7ab78f46b773b286905
Evgeny Cherkashin <eugeneai@irnok.net>
2022-06-08 13:25:22 +0800

    Added = keyword for the assignment.

--- pybars/_compiler.py
+++ pybars/_compiler.py
@@ -45,7 +45,8 @@ except NameError:
 
 
 # Flag for testing
-debug = False
+# debug = False
+debug = True
 
 
 # Note that unless we presume handlebars is only generating valid html, we have
@@ -119,6 +120,7 @@ pathseg ::= '[' <notclosebracket>+:symbol ']' => u''.join(symbol)
     | ('.' '.' '/') => u'@_parent'
     | '.' => u''
     | ' ' => u''
+    | '=' => u':'
 pathfinish :expected ::= <start> '/' <path>:found ?(found == expected) <finish>
 symbolfinish :expected ::= <start> '/' <symbol>:found ?(found == expected) <finish>
 blockrule ::= <start> '#' <block_inner>:i
